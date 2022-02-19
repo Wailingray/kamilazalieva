08e368395b6159a35c172bc817816aa70fe0eace
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2022-02-19 18:39:35 +0300

    about done

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import {Intro} from "../components/intro/Intro";
+import { Intro } from "../components/intro/Intro";
+import { About } from "../components/about/About";
 
 const App = () => {
   return (
     <div>
       <Intro />
+      <About />
     </div>
   );
 };
---
+++ src/components/about/About.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styles from "./about.module.css";
+import photoPath from "../../images/kamila_on_stage.jpg";
+import restaurantPath from "../../images/russian_wine_bar.png";
+
+export const About = () => {
+  return (
+    <section className={styles.about}>
+      <div className={styles.cardContainer}>
+        <div className={styles.background}></div>
+        <div className={styles.card}>
+          <img src={photoPath} alt="О Камиле" className={styles.photo} />
+        </div>
+      </div>
+      <div className={styles.descriptionContainer}>
+        <h1 className={styles.title}>Обо мне</h1>
+        <p className={styles.subtitle}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aut ab
+          dolores quis at consectetur nam voluptas
+        </p>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit,
+          repellat laudantium fugiat vel temporibus obcaecati minima ducimus
+          facere nam architecto reprehenderit asperiores corrupti, placeat
+          delectus tenetur doloremque consequatur illo consectetur.
+        </p>
+        <div className={styles.label}>
+          <img
+            className={styles.labelImage}
+            src={restaurantPath}
+            alt="Russian Wine Bar"
+          />
+          <div className={styles.labelInfo}>
+            <h3 className={styles.labelTitle}>Russian Wine Bar</h3>
+            <p className={styles.labelDescription}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Ut hic ex fugit similique officia quia magni
+            </p>
+          </div>
+        </div>
+        <div className={styles.label}>
+          <img
+            className={styles.labelImage}
+            src={restaurantPath}
+            alt="Russian Wine Bar"
+          />
+          <div className={styles.labelInfo}>
+            <h3 className={styles.labelTitle}>Russian Wine Bar</h3>
+            <p className={styles.labelDescription}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Ut hic ex fugit similique officia quia magni
+            </p>
+          </div>
+        </div>
+        <div className={styles.label}>
+          <img
+            className={styles.labelImage}
+            src={restaurantPath}
+            alt="Russian Wine Bar"
+          />
+          <div className={styles.labelInfo}>
+            <h3 className={styles.labelTitle}>Russian Wine Bar</h3>
+            <p className={styles.labelDescription}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Ut hic ex fugit similique officia quia magni
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
